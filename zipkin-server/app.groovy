8e42291d04f824023cc71b848ccb68e094bd5a0b
itsmurugappan <itsmurugappan@gmail.com>
2017-01-09 16:41:46 -0800

    adding tracing

---
+++ zipkin-server/app.groovy
@@ -0,0 +1,12 @@
+@Grab("spring-cloud-starter-bus-kafka")
+@Grab("zipkin-server")
+@Grab("zipkin-autoconfigure-ui")
+@Grab("spring-cloud-sleuth-zipkin-stream")
+
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer
+
+
+@EnableDiscoveryClient
+@EnableZipkinStreamServer
+class ZipKinServer {
+}
