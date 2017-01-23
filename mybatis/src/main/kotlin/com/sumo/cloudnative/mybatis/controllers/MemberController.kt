ee7f7636e16139459c6dd899da00e5794e0d4293
itsmurugappan <itsmurugappan@gmail.com>
2017-01-23 11:10:15 -0800

    adding tracing

--- mybatis/src/main/kotlin/com/sumo/cloudnative/mybatis/controllers/MemberController.kt
+++ mybatis/src/main/kotlin/com/sumo/cloudnative/mybatis/controllers/MemberController.kt
@@ -7,26 +7,39 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cloud.client.discovery.DiscoveryClient
+import org.springframework.cloud.netflix.feign.FeignClient
+
 
 
 @RestController
-class MemberController @Autowired constructor(var client: DiscoveryClient, val service: MemberService) {
+class MemberController @Autowired constructor(var client: DiscoveryClient,
+                                              val service: MemberService,
+                                              val captionClient: CaptionClient,
+                                              val intClient: IntClient,
+                                              val addClient: AddClient) {
 
     @RequestMapping("/")
     fun hello(): String {
         val localInstance = client.localServiceInstance
+        captionClient.caption();
+        intClient.int();
         return "MyBatis: " + localInstance.serviceId + ":" + localInstance.host + ":" + localInstance.port
     }
 
     //@RequestMapping("members",  method = arrayOf(RequestMethod.GET))
     @GetMapping("members")
     fun members(): List<Member>  {
+        intClient.int();
+        captionClient.caption();
+        addClient.add();
         return service.findAll();
     }
 
     @GetMapping("members/{id}")
     fun member(@PathVariable id: Int): Member {
         print(id)
+        intClient.int();
+        captionClient.caption();
         return service.findById(id);
     }
 }
