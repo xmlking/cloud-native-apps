dbae0482042a17a3ece1f5f16749feac611a78ef
itsmurugappan <itsmurugappan@gmail.com>
2017-03-01 11:24:22 -0800

    consul and vault

--- zipkin/app.groovy
+++ zipkin/app.groovy
@@ -1,3 +1,7 @@
+@Grab("org.springframework.kafka:spring-kafka:1.1.1.RELEASE")
+@Grab("org.springframework.integration:spring-integration-kafka:2.1.0.RELEASE")
+@Grab("org.apache.kafka:kafka_2.11:0.10.0.0")
+@GrabExclude("org.slf4j:slf4j-log4j12")
 @Grab("spring-cloud-starter-bus-kafka")
 @Grab("zipkin-server")
 @Grab("zipkin-autoconfigure-ui")
