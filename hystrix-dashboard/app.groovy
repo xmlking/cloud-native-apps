d72af345c3a5760ac3b7722b20d19d10f57640db
itsmurugappan <itsmurugappan@gmail.com>
2017-03-01 11:25:23 -0800

    consul and vault

--- hystrix-dashboard/app.groovy
+++ hystrix-dashboard/app.groovy
@@ -1,4 +1,12 @@
+@Grab("org.springframework.kafka:spring-kafka:1.1.1.RELEASE")
+@Grab("org.springframework.integration:spring-integration-kafka:2.1.0.RELEASE")
+@Grab("org.apache.kafka:kafka_2.11:0.10.0.0")
+@GrabExclude("org.slf4j:slf4j-log4j12")
 @Grab("spring-cloud-starter-bus-kafka")
+@Grab("spring-cloud-sleuth-stream")
+@Grab("spring-cloud-starter-consul-discovery")
+@Grab("spring-cloud-starter-hystrix")
+//@Grab("spring-cloud-starter-consul-config")
 @Grab("spring-cloud-starter-hystrix-dashboard")
 @groovy.transform.CompileStatic
 @EnableDiscoveryClient
